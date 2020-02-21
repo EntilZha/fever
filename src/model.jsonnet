befd89144f19d8a4f9269cfcbcc3f2e6c2d742a7
Pedro Rodriguez <ski.rodriguez@gmail.com>
2020-02-20 19:36:32 -0500

    Initial claim only fever

---
+++ src/model.jsonnet
@@ -0,0 +1,27 @@
+function(lr=0.0001) {
+  dataset_reader: {
+    type: 'fever',
+  },
+  train_data_path: 'data/train.jsonl',
+  validation_data_path: 'data/shared_task_dev.jsonl',
+  model: {
+    type: 'claim_only',
+    dropout: 0.25,
+  },
+  iterator: {
+    type: 'bucket',
+    sorting_keys: [['text', 'num_tokens']],
+    batch_size: 32,
+  },
+  trainer: {
+    optimizer: {
+      type: 'adam',
+      lr: lr,
+    },
+    validation_metric: '+accuracy',
+    num_serialized_models_to_keep: 1,
+    num_epochs: 50,
+    patience: 2,
+    cuda_device: 0,
+  },
+}
---
+++ src/snif/util.py
@@ -0,0 +1,25 @@
+import subprocess
+import logging
+import os
+
+
+def shell(command):
+    return subprocess.run(command, check=True, shell=True, stderr=subprocess.STDOUT)
+
+
+def safe_path(path):
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    return path
+
+
+def get_logger(name):
+    log = logging.getLogger(name)
+
+    if not log.hasHandlers():
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        sh = logging.StreamHandler()
+        sh.setLevel(logging.INFO)
+        sh.setFormatter(formatter)
+        log.addHandler(sh)
+        log.setLevel(logging.INFO)
+    return log
